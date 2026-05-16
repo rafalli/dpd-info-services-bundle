8dfadda6c0ed8239fddd683ef311e142a8b2d760
Rafał Linca <rafal.linca@gmail.com>
2026-05-16 15:39:51 +0200

    fix(config): hint ArrayNodeDefinition for PHPStan on Symfony 6.4

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rafalli\DpdInfoServicesBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -13,27 +14,30 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('rafalli_dpd_info_services');
 
-        $treeBuilder->getRootNode()
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
             ->children()
-            ->scalarNode('wsdl_url')
-            ->defaultValue('https://dpdservices.dpd.com.pl/DPDInfoServicesObjEventsService/DPDInfoServicesObjEvents?wsdl')
-            ->info('WSDL address for production or test environment')
-            ->end()
-            ->scalarNode('channel')
-            ->isRequired()
-            ->cannotBeEmpty()
-            ->info('Channel (Customer Information Channel ID) assigned by DPD')
-            ->end()
-            ->scalarNode('username')
-            ->isRequired()
-            ->cannotBeEmpty()
-            ->info('DPD InfoServices API login')
-            ->end()
-            ->scalarNode('password')
-            ->isRequired()
-            ->cannotBeEmpty()
-            ->info('DPD InfoServices API password')
-            ->end()
+                ->scalarNode('wsdl_url')
+                    ->defaultValue('https://dpdinfoservices.dpd.com.pl/DPDInfoServicesObjEventsService/DPDInfoServicesObjEvents?wsdl')
+                    ->info('WSDL address for production or test environment')
+                ->end()
+                ->scalarNode('channel')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->info('Channel (PID) assigned by DPD')
+                ->end()
+                ->scalarNode('username')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->info('DPD InfoServices API login')
+                ->end()
+                ->scalarNode('password')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->info('DPD InfoServices API password')
+                ->end()
             ->end()
         ;
 
